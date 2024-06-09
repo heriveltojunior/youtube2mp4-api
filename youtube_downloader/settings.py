54debca37defacbc0973c537f3ea4e722eadd2e7
Herivelto Junior <herijr@gmail.com>
2024-06-09 02:12:17 -0300

    Adicionado arquivo requirements.txt

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # URL do frontend React
 ]
 
 ROOT_URLCONF = 'youtube_downloader.urls'
