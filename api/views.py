ac621b7f3bd2f07dfbe39c23719397dae13057e1
Herivelto Junior <herijr@gmail.com>
2024-08-28 16:31:34 -0300

    Subustituir Pytube por yt-dlp

--- api/views.py
+++ api/views.py
@@ -1,33 +1,45 @@
 # api/views.py
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from pytube import YouTube
-import os
 from django.conf import settings
 from django.http import JsonResponse
-from django.views import View
-from django.shortcuts import render
+from rest_framework.views import APIView
+from yt_dlp import YoutubeDL
+import ffmpeg
+import os
 
 class DownloadVideoView(APIView):
-    def post(self, request, *args, **kwargs):
-        video_url = request.data.get("url")
-        if not video_url:
-            return Response({"error": "URL do vídeo é necessária"}, status=status.HTTP_400_BAD_REQUEST)
-        
+    def post(self, request):
+        url = request.data.get('url')
+        if not url:
+            return JsonResponse({'error': 'URL is required'}, status=400)
+
         try:
-            yt = YouTube(video_url)
-            stream = yt.streams.filter(progressive=True, file_extension='mp4',resolution='360p').order_by('resolution').desc().first()
-            file_path = stream.download(output_path=settings.MEDIA_ROOT)
-            file_name = os.path.basename(file_path)
-            download_url = request.build_absolute_uri(settings.MEDIA_URL + file_name)
-            title = yt.title
-            thumb_video = yt.thumbnail_url
-            
-            return JsonResponse({"download_url": download_url, "title": title, "thumb_video": thumb_video}, status=status.HTTP_200_OK)
+            ydl_opts = {
+                'outtmpl': os.path.join(settings.MEDIA_ROOT, '%(title)s.%(ext)s'),
+                'format': 'bestvideo[height<=360]+bestaudio/best[height<=360]',
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=True)
+                title = info_dict.get('title', None)
+                thumb_video = info_dict.get('thumbnail', None)
+                video_title = title.replace(' ', '_')
+                video_title = video_title.replace('#', '_')
+                video_title = video_title.replace('?', '_')
+                video_filename = ydl.prepare_filename(info_dict)
+
+            mp4_path = os.path.join(settings.MEDIA_ROOT, f"{video_title}.mp4")
+            if os.path.exists(mp4_path):
+                converter = False
+            else:
+                converter = True
+
+            if converter:
+                ffmpeg.input(video_filename).output(mp4_path).overwrite_output().run()
+
+            download_url = request.build_absolute_uri(settings.MEDIA_URL + f"{video_title}.mp4")
+            return JsonResponse({"download_url": download_url, "title": title, "thumb_video": thumb_video})
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({'error': str(e)}, status=500)
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
